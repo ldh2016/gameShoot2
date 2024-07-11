00d194c3d940b49e3ce481536a135ce5048a28a2
aike2048 <446845138@qq.com>
2024-07-11 11:39:40 +0800

    提交测试

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-window.screenOrientation="sensor_portrait",loadLib("libs/laya.core.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("libs/laya.physics3D.js"),loadLib("zsSdk/zs.sdk.js"),loadLib("js/bundle.js");
+window.screenOrientation="sensor_portrait",loadLib("libs/laya.core.js"),loadLib("libs/laya.core.js"),loadLib("libs/laya.core.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("libs/laya.physics3D.js"),loadLib("zsSdk/zs.sdk.js"),loadLib("js/bundle.js");
